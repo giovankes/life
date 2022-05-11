d119bc74425b6be76743efa44761beb3b5e0b3b8
giovankes <hello@giotje.dev>
2022-05-11 12:36:21 +0200

    setup Websocket and express app

--- src/server.js
+++ src/server.js
@@ -1,14 +1,49 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import WebSocket from 'ws';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+require('events').EventEmitter.defaultMaxListeners = 15;
+
+dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json());
+const { uri } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('lmao');
+const urlencodedParser = bodyParser.urlencoded({
+  extended: true,
 });
 
-app.listen(5000, () => {
-  console.log('app is listening on port 5000');
+const jsonParser = bodyParser.json();
+
+const wss = new WebSocket.Server({
+  port: 2611,
+});
+
+app.use(
+  express.json({
+    limit: '100mb',
+  })
+);
+
+const server = app.listen(8082, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('express server started at: http://%s:%s', host, port);
+});
+
+// handles websockets for inserting data
+
+const clients = new Map();
+
+wss.on('connection', (wss, request, client) => {
+  clients.set(wss);
+  wss.on('message', (data) => {
+    const message = JSON.parse(data);
+
+    // insert message
+  });
 });
