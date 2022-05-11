894244824299aa2521f9dc96b8dfb659b855858c
giovankes <hello@giotje.dev>
2022-05-11 15:05:15 +0200

    added db insert many data

--- src/functions/database.js
+++ src/functions/database.js
@@ -22,4 +22,13 @@ function db_insert(collection, data) {
   });
 }
 
-export { db_insert };
+function db_insert_many(collection, data) {
+  connect.then(() => {
+    dbo.collection(collection).insertMany(data, (err) => {
+      if (err) throw err;
+      consola.info('successfully inserted data');
+    });
+  });
+}
+
+export { db_insert, db_insert_many };
