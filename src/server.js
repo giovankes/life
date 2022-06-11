c2db5ddc6f53e3978bc1c692c3b82548bf4a9e8d
giovankes <hello@giotje.dev>
2022-06-11 13:21:39 +0200

    push to forest

--- src/server.js
+++ src/server.js
@@ -16,29 +16,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // NOTE: * custom middleware * //
-//app.use(async (req, res, next) => {
-//  req.body = {
-//    ...req.body,
-//  };
-//  req.context = {
-//    models,
-//    me: await models.User.findByLogin('giovankes'),
-//  };
-//  next();
-//});
+app.use(async (req, res, next) => {
+  req.body = {
+    ...req.body,
+  };
+  req.context = {
+    models,
+    me: await models.User.findByLogin('giovankes'),
+  };
+  next();
+});
 
 // NOTE: * Routes * //
 
-//app.use('/session', routes.session);
-//app.use('/users', routes.user);
-//app.use('/messages', routes.message);
-//app.use('/transport', routes.transport);
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+app.use('/transport', routes.transport);
 
-//connectDb().then(async () => {
-//  app.listen(PORT || 261120, () => {
-//    consola.info(`app is listening on port: ${PORT}`);
-//  });
-//});
+connectDb().then(async () => {
+  app.listen(PORT || 261120, () => {
+    consola.info(`app is listening on port: ${PORT}`);
+  });
+});
 
 // NOTE: * seed the database * //
 
@@ -47,7 +47,3 @@ app.use(cors());
 //NOTE: * hooks * //
 //spotify();
 //spotify_hook();
-
-app.listen(12345, () => {
-  consola.info(`app is listening on port: 261120`);
-});
