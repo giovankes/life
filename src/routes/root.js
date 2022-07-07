68f68408b96c75edd057f0e26c198ed2dd80f3e3
Giotje <devgio@icloud.com>
2022-07-07 08:38:25 +0200

    added root route

--- src/routes/root.js
+++ src/routes/root.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get('/', async (req, res) => {
   try {
     log('root', req);
-    consola.info('root route was just called');
+    console.log('root route was just called');
     return res.send('Hello world!');
   } catch (err) {
     console.error(err);
